5f35330a40af1b9f7d4b60092cd7dc317d7e8e99
Utkarsh <70141321+utkars8055@users.noreply.github.com>
2023-11-03 08:17:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ from pymongo.collection import Collection
 
 dotenv.load_dotenv(".env")
 
-api_id = os.environ.get('API_ID')
-api_hash = os.environ.get('API_HASH')
-bot_token = os.environ.get('BOT_TOKEN')
+api_id = os.environ.get('25276771')
+api_hash = os.environ.get('9b9b4b689989390be2ab0889f0e8e459')
+bot_token = os.environ.get('6897949043:AAFJR4a0g7wKydGh0u10v3Ct0uNCRgFNq4A')
 db_url = os.environ.get('MONGO_DB_URL')
 database_name = os.environ.get('DATABASE_NAME')
 owner_id = int(os.environ.get('OWNER_ID'))
